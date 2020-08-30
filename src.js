ed07aac8d05afeb957fd4d9a05cb89e0447e0b58
S.S.Srilakshmi <srilakshmi@student.tce.edu>
2020-08-30 10:49:00 +0530

    personal website completed with simple html,css,js

--- src.js
+++ src.js
@@ -1,2 +1,46 @@
-
+//welcome users
 window.alert("Welcome to my website");
+//Displaying full form of HTML when mouse over the text 
+function m1Over(obj)
+{
+    obj.innerHTML="Hyper Text Markup Language (HTML) "
+}
+//Displaying HTML as it is when mouse out of the text
+function m1Out(obj){
+    obj.innerHTML="HTML"
+}
+
+
+//Displaying full form of CSS when mouse over the text
+function m2Over(obj)
+{
+    obj.innerHTML="Cascading Style Sheets (CSS) "
+}
+
+//Displaying CSS as it is when mouse out of the text
+
+function m2Out(obj){
+    obj.innerHTML="CSS"
+}
+
+//Displaying full form of PHP when mouse over the text
+function m3Over(obj)
+{
+    obj.innerHTML="Hypertext Preprocessor (PHP) "
+}
+
+//Displaying PHP as it is when mouse out of the text
+function m3Out(obj){
+    obj.innerHTML="PHP"
+}
+
+//Displaying full form of SQL when mouse over the text
+function m4Over(obj)
+{
+    obj.innerHTML="Structured Query Language (SQL) "
+}
+
+//Displaying SQL as it is when mouse out of the text
+function m4Out(obj){
+    obj.innerHTML="SQL"
+}
